29b247e4ea98a6de67ba63f9c18db5764ef55843
DelDmc <98178667+DelDmc@users.noreply.github.com>
2023-03-25 16:38:36 +0200

    Refactor as per comments
    - renamed variables
    - moved query selectors to separate variables
    - separated variables assignment and logic

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 const headerDiv = document.querySelector('div.lessons-list-timeline__header');
-const allDefault = document.querySelectorAll(".lessons-timeline__item-link--default");
-const allSuccess = document.querySelectorAll(".lessons-timeline__item-link--success");
-const allDanger = document.querySelectorAll(".lessons-timeline__item-link--danger");
-const allHolder = document.querySelectorAll("span.lessons-timeline__item-holder");
+const defaultElements = document.querySelectorAll(".lessons-timeline__item-link--default");
+const successElements = document.querySelectorAll(".lessons-timeline__item-link--success");
+const warningElements = document.querySelectorAll(".lessons-timeline__item-link--warning");
+const dangerElements = document.querySelectorAll(".lessons-timeline__item-link--danger");
+const holderElements = document.querySelectorAll("span.lessons-timeline__item-holder");
+const HWElements = document.querySelectorAll(`app-lesson-header > div.lesson-header-top.ng-star-inserted > app-lesson-badge > span:nth-child(3)`);
+const PassedHWsElements = document.querySelectorAll("article > header > app-lesson-header > div.lesson-header-top.ng-star-inserted > app-lesson-badge > span:nth-child(1)");
 
-const lastLesson = +allHolder.item(allHolder.length - 1).textContent;
-const lessonsPassed = allDefault.length + allSuccess.length + allDanger.length;
+const lastLesson = +holderElements.item(holderElements.length - 1).textContent;
+const lessonsPassed = defaultElements.length + successElements.length + warningElements.length + dangerElements.length;
 const lessonsLeft = lastLesson - lessonsPassed;
-
-let totalHWs = Array.from(document.querySelectorAll(`app-lesson-header > div.lesson-header-top.ng-star-inserted > app-lesson-badge > span:nth-child(3)`))
-  .reduce((acc, element) => acc + (+element.textContent), 0);
-
-let passedHWsNumber = Array.from(document.querySelectorAll("article > header > app-lesson-header > div.lesson-header-top.ng-star-inserted > app-lesson-badge > span:nth-child(1)"))
-  .reduce((acc, element) => acc + (+element.textContent), 0);
-
-const toPassHWs = totalHWs - passedHWsNumber;
+const totalHWs = Array.from(HWElements).reduce((acc, element) => acc + (+element.textContent), 0);
+const passedHWsNumber = Array.from(PassedHWsElements).reduce((acc, element) => acc + (+element.textContent), 0);
+const toPassHWS = totalHWs - passedHWsNumber;
 
 const headerMod = `
   <div _ngcontent-hillel_lms_server-c218="" class="lessons-list-timeline__header">
     <span>Пройдено занять: ${lessonsPassed}</span>
     <span>Залишилось занять: ${lessonsLeft}</span>
     <span>Здано домашок: ${passedHWsNumber}</span>
-    <span>Залишилось здати домашок: ${toPassHWs}</span>
+    <span>Залишилось здати домашок: ${toPassHWS}</span>
   </div>`;
 headerDiv.insertAdjacentHTML('afterend', headerMod);
